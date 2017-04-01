79c802a2f379d8c9b39f8c5adbc75f2fe723f1e0
Elliptica <elliptica.magnificum@gmail.com>
2017-04-01 13:23:07 +0300

    Rename keywords → settings and add alias for settings

--- test/repl.py
+++ test/repl.py
@@ -10,21 +10,21 @@ from margo import type_checking
 class REPL(cmd.Cmd):
     intro = "This is Adrian testing REPL.\n"
     prompt = ">>> "
-    keywords = {"exit_on_error": True}
+    settings = {"exit_on_error": True}
 
-    def do_keywords(self, keywords):
-        if keywords:
-            for kw in keywords.split(" "):
+    def do_settings(self, settings):
+        if settings:
+            for kw in settings.split(" "):
                 key, value = kw.split("=")
-                self.keywords[key] = eval(value)
+                self.settings[key] = eval(value)
         else:
-            print(self.keywords)
+            print(self.settings)
 
     def do_eval(self, inp):
         try:
-            ast = lex_parse.main(inp, **self.keywords)
-            nc_ast = name_checking.main(ast, **self.keywords)
-            tc_ast = type_checking.main(nc_ast, **self.keywords)
+            ast = lex_parse.main(inp, **self.settings)
+            nc_ast = name_checking.main(ast, **self.settings)
+            tc_ast = type_checking.main(nc_ast, **self.settings)
             print(tc_ast)
         except errors.CompilationError as e:
             pass
@@ -36,7 +36,7 @@ class REPL(cmd.Cmd):
         self.do_eval(inp)
 
     do_quit = do_exit
-    do_kw = do_keywords
+    do_set = do_settings
 
 
 if __name__ == "__main__":
