41cd794ac0b0d69c9161c9f66e725a181d3ffb0e
Elliptica <elliptica.magnificum@gmail.com>
2017-04-23 15:13:29 +0300

    Refactor std alias layer

--- margo/std_alias.py
+++ margo/std_alias.py
@@ -11,31 +11,69 @@ _FUNCS = funcreg.TypeRegistry()
 def type_from_alias(type_, *, context):
     if (isinstance(type_, ast.Name) and \
             type_.value in defs.STD_TYPE_NAMES):
-        return ast.ModuleMember("std_types", type_)
+        return ast.ModuleMember(defs.STD_TYPES_MODULE_NAME, type_)
     return type_
 
 
-def translate_atom_to_init(atom):
+def name_to_copy_method(name, *, context):
+    if isinstance(name, ast.Name):
+        type_ = context.namespace.get(name.value)["type"]
+        return ast.MethodCall(
+            ast.StructElem(type_, ast.Name("copy")), args=[name])
+    errors.not_implemented(context.line, context.exit_on_error)
+
+
+def method_call(call, *, context):
+    type_ = call.method
+    if not isinstance(type_, ast.StructElem):
+        return ast.MethodCall(
+            ast.StructElem(type_, ast.Name("init")), call.args)
+    elif type_.elem.value == "init":
+        # Bug can be here!
+        return call
+    errors.not_implemented(context.line, context.exit_on_error)
+
+
+def std_type_init_args_from_alias(args, *, context):
+    result = []
+    for arg in args:
+        if isinstance(arg, ast.Integer):
+            # We usually can calculate length of an Integer when compiling.
+            result.append(ast.MethodCall(
+                ast.StructElem(ast.ModuleMember(
+                    defs.C_MODULE_NAME, defs.C_CSTRING), ast.Name("init")),
+                [arg.value]))
+        else:
+            errors.not_implemented(context.line, context.exit_on_error)
+    return result
+
+
+def atom_to_init_method(atom, *, context):
+    type_ = ast.Name(atom.to_string())
+    args = std_type_init_args_from_alias([atom], context=context)
     return ast.MethodCall(ast.StructElem(
-        ast.ModuleMember(defs.STD_TYPES_MODULE_NAME, ast.Name(atom.to_string())),
-        ast.Name("init")), args=[atom.value])
+        ast.ModuleMember(defs.STD_TYPES_MODULE_NAME, type_),
+        ast.Name("init")), args)
+    errors.not_implemented(context.line, context.exit_on_error)
 
 
-def expr_from_aliases(expr, *, context):
-    # TODO: translate `1 + 2` to
-    # `std_types#Integer.add(std_types#Integer.init(1), ...)`
-    if isinstance(expr, (ast.Name, ast.ModuleMember, ast.MethodCall)):
-        return expr
+def expr_from_alias(expr, *, context):
+    if isinstance(expr, ast.Name):
+        return name_to_copy_method(expr, context=context)
+    elif isinstance(expr, ast.MethodCall):
+        return method_call(expr, context=context)
     elif isinstance(expr, defs.ATOM_TYPES):
-        return translate_atom_to_init(expr)
+        return atom_to_init_method(expr, context=context)
     errors.not_implemented(context.line, context.exit_on_error)
 
 
 @_FUNCS.register(ast.Decl)
 def decl(stmt, *, context):
     type_ = type_from_alias(stmt.type_, context=context)
-    expr = expr_from_aliases(stmt.expr, context=context)
-    return ast.Decl(stmt.name, type_, expr)
+    context.namespace.add_name(stmt.name.value, {"type": type_})
+    return ast.Decl(
+        stmt.name, type_,
+        expr_from_alias(stmt.expr, context=context))
 
 
 def translate(ast_, *, context):
