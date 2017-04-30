09c9a8f7bcd9fe25daedfdb36152784746431eb4
Elliptica <elliptica.magnificum@gmail.com>
2017-04-30 07:32:32 +0300

    Replace old versions of name checking and naming rules layers by alternative versions

--- margo/name_existence.py
+++ margo/name_existence.py
@@ -2,123 +2,114 @@ import pathlib
 
 from . import ast
 from . import defs
+from . import layers
 from . import errors
 
-from vendor.paka import funcreg
+
+class NameExistence(layers.Layer):
+
+    def _name_exists(self, name):
+        return (name in defs.STD_FUNCS or \
+                self.context.namespace.exists(name))
+
+    def _type_exists(self, type_):
+        return (type_ in defs.STD_TYPE_NAMES or \
+                self.context.typespace.exists(type_))
+
+    def _module_exists(self, module_name):
+        if module_name == defs.C_MODULE_NAME:
+            return True
+        elif module_name in defs.STD_MODULES_NAMES:
+            return True
+        for path in self.context.module_paths:
+            for module in pathlib.Path(path).iterdir():
+                if module_name == module.name:
+                    return True
+        return False
 
 
-_FUNCS = funcreg.TypeRegistry()
-
-
-def name_in_module(name, module_name, *, context):
-    # TODO: parse module, create a context for this module, search for name.
-    return True
-
-
-def get_module_path(module_name, *, context):
-    for path in context.module_paths:
-        for module in pathlib.Path(path).iterdir():
-            if module_name == module.name:
-                return module
-    if module_name == defs.C_MODULE_NAME:
+    def _module_name(self, module_name):
+        if not self._module_exists(module_name):
+            errors.non_existing_module(
+                self.context.line, self.context.exit_on_error, module_name)
         return module_name
-    elif module_name in defs.STD_MODULE_NAMES:
-        return defs.STD_MODULES_PATH + module_name + defs.ADRIAN_FILE_EXTENSION
-    errors.non_existing_module(
-        context.line, context.exit_on_error, module=module_name)
 
+    def _decl_type(self, type_):
+        if isinstance(type_, ast.Name):
+            if not self._type_exists(type_.value):
+                errors.non_existing_type(
+                    self.context.line, self.context.exit_on_error,
+                    type_.value)
+            return type_
+        elif isinstance(type_, ast.ModuleMember):
+            # TODO: think about type in module
+            return ast.ModuleMember(
+                self._module_name(type_.module_name), type_.member)
+        elif isinstance(type_, ast.StructElem):
+            return ast.StructElem(
+                self._decl_type(type_.struct),
+                self._name(type_.elem))
+        errors.not_implemented(
+            self.context.line, self.context.exit_on_error)
 
-def add_include(module_name, module_path, *, context):
-    if (not module_name in context.includes and \
-            module_name != defs.C_MODULE_NAME):
-        context.includes[module_name] = module_path
+    def _decl_name(self, name):
+        # If std name.
+        if name.value in defs.STD_FUNCS:
+            errors.cant_reassign_builtin(
+                self.context.line, self.context.exit_on_error, name.value)
+        return name
 
-
-def check_module_existence(module_name, *, context):
-    path = get_module_path(module_name, context=context)
-    add_include(module_name, path, context=context)
-
-
-def check_struct_field_or_method_existence(struct, *, context):
-    if struct.elem == "init":
-        pass
-
-
-def check_type_existence(type_, *, context):
-    if isinstance(type_, ast.Name):
-        if not (type_.value in defs.STD_TYPE_NAMES or \
-                context.typespace.exists(type_.value)):
-            errors.non_existing_type(
-                context.line, context.exit_on_error, name=type_.value)
-    elif isinstance(type_, ast.ModuleMember):
-        check_module_existence(type_.module_name, context=context)
-        if not name_in_module(
-                type_.member, type_.module_name, context=context):
-            errors.cant_find_name_in_module(
-                context.line, context.exit_on_error,
-                name=type_.member, module_name=type_.module_name)
-    elif isinstance(type_, ast.StructElem):
-        check_type_existence(type_.struct, context=context)
-        check_struct_field_or_method_existence(type_, context=context)
-    else:
-        errors.not_implemented(context.line, context.exit_on_error)
-
-
-def check_name_existence(name, *, context):
-    if isinstance(name, ast.Name):
-        if not context.namespace.exists(name.value):
+    def _name(self, name):
+        # If name exists.
+        if isinstance(name, ast.Name):
+            if self._name_exists(name.value):
+                return name
             errors.non_existing_name(
-                context.line, context.exit_on_error, name=name.value)
-    elif isinstance(name, ast.ModuleMember):
-        check_module_existence(name.module_name, context=context)
-        if not name_in_module(
-                name.member, name.module_name, context=context):
-            errors.cant_find_name_in_module(
-                context.line, context.exit_on_error,
-                name=name.member, module_name=name.module_name)
-    else:
-        errors.not_implemented(context.line, context.exit_on_error)
+                self.context.line, self.context.exit_on_error, name.value)
+        errors.not_implemented(
+            self.context.line, self.context.exit_on_error)
 
+    def _call_args(self, args):
+        result = []
+        for arg in args:
+            result.append(self._expr(arg))
+        return result
 
-def check_call_args(args, *, context):
-    for arg in args:
-        check_expr(arg, context=context)
+    def _expr(self, expr):
+        if isinstance(expr, ast.Name):
+            return self._name(expr)
+        elif isinstance(expr, ast.MethodCall):
+            if isinstance(expr.method, ast.StructElem):
+                if isinstance(expr.method.elem, ast.Name):
+                    return ast.MethodCall(
+                        ast.StructElem(
+                            self._decl_type(expr.method.struct),
+                            expr.method.elem),
+                        self._call_args(expr.args))
+            return ast.MethodCall(
+                self._decl_type(expr.method), self._call_args(expr.args))
+        elif isinstance(expr, defs.ATOM_TYPES):
+            # No names here.
+            return expr
+        elif isinstance(expr, list):
+            return [
+                expr[0],
+                self._expr(expr[1]),
+                self._expr(expr[2])
+            ]
+        errors.not_implemented(
+            self.context.line, self.context.exit_on_error)
 
-
-def check_expr(expr, *, context):
-    if isinstance(expr, (ast.Name, ast.ModuleMember)):
-        check_name_existence(expr, context=context)
-    elif isinstance(expr, ast.MethodCall):
-        check_type_existence(expr.method, context=context)
-        check_call_args(expr.args, context=context)
-    elif isinstance(expr, defs.ATOM_TYPES):
-        pass
-    elif isinstance(expr, list):
-        check_expr(expr[1], context=context)
-        check_expr(expr[2], context=context)
-    else:
-        errors.not_implemented(context.line, context.exit_on_error)
-
-
-@_FUNCS.register(ast.Decl)
-def decl(stmt, *, context):
-    if stmt.name.value in defs.STD_FUNCS:
-        errors.cant_reassign_builtin(
-            context.line, context.exit_on_error, name=stmt.name.value)
-    if stmt.type_:
-        check_type_existence(stmt.type_, context=context)
-    if stmt.expr:
-        check_expr(stmt.expr, context=context)
-    context.namespace.add_name(
-        stmt.name.value, {"node_type": defs.NodeType.variable})
-
-
-def check(ast_, *, context):
-    for pair in ast_:
-        context.line = pair.line
-        _FUNCS[pair.stmt](pair.stmt, context=context)
-
-
-def main(ast_, *, context=ast.Context(
-        exit_on_error=True, module_paths=[defs.STD_MODULES_PATH])):
-    check(ast_, context=context)
+    def decl(self, stmt):
+        self.name = self._decl_name(stmt.name)
+        self.type_ = stmt.type_
+        if stmt.type_:
+            self.type_ = self._decl_type(stmt.type_)
+        self.expr = stmt.expr
+        if stmt.expr:
+            self.expr = self._expr(stmt.expr)
+        # Register here to avoid linking in expression to itself.
+        self.context.namespace.add_name(self.name, {
+            "node_type": defs.NodeType.variable
+        })
+        return ast.Decl(self.name, self.type_, self.expr)
--- margo/naming_rules.py
+++ margo/naming_rules.py
@@ -1,112 +1,85 @@
-import re
-import pathlib
-
 from . import ast
 from . import defs
+from . import layers
 from . import errors
 
-from vendor.paka import funcreg
 
+class NamingRules(layers.Layer):
 
-_FUNCS = funcreg.TypeRegistry()
+    def _matches_variable_name(self, name):
+        return defs.VARIABLE_REGEX.fullmatch(name)
 
+    def _matches_type_name(self, name):
+        return defs.TYPE_REGEX.fullmatch(name)
 
-def _matches_name(name, regex):
-    return regex.fullmatch(name)
+    def _matches_module_name(self, name):
+        return defs.MODULE_REGEX.fullmatch(name)
 
-
-def matches_variable_name(name):
-    return _matches_name(name, defs.VARIABLE_REGEX)
-
-
-def matches_type_name(name):
-    return _matches_name(name, defs.TYPE_REGEX)
-
-
-def matches_module_name(name):
-    return _matches_name(name, defs.MODULE_REGEX)
-
-
-def check_decl_name(name, *, context):
-    if not matches_variable_name(name.value):
-        errors.bad_name_for_variable(
-            context.line, context.exit_on_error, name=name.value)
-
-
-def check_module_name(name, *, context):
-    if not matches_module_name(name):
-        errors.bad_name_for_module(
-            context.line, context.exit_on_error, name=name)
-
-
-def check_type_name(name, *, context):
-    if not matches_type_name(name.value):
-        errors.bad_name_for_type(
-            context.line, context.exit_on_error, name=name.value)
-
-
-def check_decl_type(type_, *, context):
-    if isinstance(type_, ast.Name):
-        check_type_name(type_, context=context)
-    elif isinstance(type_, ast.ModuleMember):
-        check_module_name(type_.module_name, context=context)
-        check_decl_type(type_.member, context=context)
-    elif isinstance(type_, ast.MethodCall):
-        check_decl_type(type_.method, context=context)
-    elif isinstance(type_, ast.StructElem):
-        check_decl_type(type_.struct, context=context)
-        check_name(type_.elem, context=context)
-    else:
-        errors.not_implemented(context.line, context.exit_on_error)
-
-
-def check_call_args(args, *, context):
-    for arg in args:
-        check_expr(arg, context=context)
-
-
-def check_name(expr, *, context):
-    if isinstance(expr, ast.Name):
-        if not matches_variable_name(expr.value):
+    def _decl_name(self, name):
+        if not self._matches_variable_name(name.value):
             errors.bad_name_for_variable(
-                context.line, context.exit_on_error, name=expr.value)
-    elif isinstance(expr, ast.ModuleMember):
-        check_module_name(expr.module_name, context=context)
-        check_name(expr.member, context=context)
-    else:
-        errors.not_implemented(context.line, context.exit_on_error)
+                self.context.line, self.context.exit_on_error, name.value)
+        return name
 
+    def _type_name(self, name):
+        if not self._matches_type_name(name.value):
+            errors.bad_name_for_type(
+                self.context.line, self.context.exit_on_error, name.value)
+        return name
 
-def check_expr(expr, *, context):
-    if isinstance(expr, (ast.Name, ast.ModuleMember)):
-        check_name(expr, context=context)
-    elif isinstance(expr, defs.ATOM_TYPES):
-        pass
-    elif isinstance(expr, list):
-        check_expr(expr[1], context=context)
-        check_expr(expr[2], context=context)
-    elif isinstance(expr, ast.MethodCall):
-        check_decl_type(expr.method, context=context)
-        check_call_args(expr.args, context=context)
-    else:
-        errors.not_implemented(context.line, context.exit_on_error)
+    def _module_name(self, name):
+        if not self._matches_module_name(name):
+            errors.bad_name_for_module(
+                self.context.line, self.context.exit_on_error, name)
+        return name
 
+    def _decl_type(self, type_):
+        if isinstance(type_, ast.Name):
+            return self._type_name(type_)
+        elif isinstance(type_, ast.ModuleMember):
+            return ast.ModuleMember(
+                self._module_name(type_.module_name),
+                self._decl_type(type_.member))
+        elif isinstance(type_, ast.StructElem):
+            return ast.StructElem(
+                self._decl_type(type_.struct),
+                self._name(type_.elem))
+        errors.not_implemented(self.context.line, self.context.exit_on_error)
 
-@_FUNCS.register(ast.Decl)
-def decl(stmt, *, context):
-    check_decl_name(stmt.name, context=context)
-    if stmt.type_:
-        check_decl_type(stmt.type_, context=context)
-    if stmt.expr:
-        check_expr(stmt.expr, context=context)
+    def _name(self, name):
+        if not self._matches_variable_name(name.value):
+            errors.bad_name_for_variable(
+                self.context.line, self.context.exit_on_error, name.value)
+        return name
 
+    def _call_args(self, args):
+        result = []
+        for arg in args:
+            result.append(self._expr(arg))
+        return result
 
-def check(ast_, *, context):
-    for pair in ast_:
-        context.line = pair.line
-        _FUNCS[pair.stmt](pair.stmt, context=context)
+    def _expr(self, expr):
+        if isinstance(expr, ast.Name):
+            return self._name(expr)
+        elif isinstance(expr, defs.ATOM_TYPES):
+            return expr
+        elif isinstance(expr, list):
+            return [
+                expr[0],
+                self._expr(expr[1]),
+                self._expr(expr[2])
+            ]
+        elif isinstance(expr, ast.MethodCall):
+            return ast.MethodCall(
+                self._decl_type(expr.method), self._call_args(expr.args))
+        errors.not_implemented(self.context.line, self.context.exit_on_error)
 
-
-def main(ast_, *, context=ast.Context(
-        exit_on_error=True, module_paths=[defs.STD_MODULES_PATH])):
-    check(ast_, context=context)
+    def decl(self, stmt):
+        self.name = self._decl_name(stmt.name)
+        self.type_ = stmt.type_
+        if stmt.type_:
+            self.type_ = self._decl_type(stmt.type_)
+        self.expr = stmt.expr
+        if stmt.expr:
+            self.expr = self._expr(stmt.expr)
+        return ast.Decl(self.name, self.type_, self.expr)
