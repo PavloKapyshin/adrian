d479938c8654e53af963d54e5fd55c434c9ae613
Elliptica <elliptica.magnificum@gmail.com>
2017-06-07 23:22:37 +0300

    Refactor tests

--- test/test_analyzer.py
+++ test/test_analyzer.py
@@ -7,144 +7,123 @@ def compile(inp):
     pseudo_result = analyzer.Analyzer(
         ast.Context(exit_on_error=True)).main(
             lex_parse.main(inp, exit_on_error=True))
-    # We don't need line info.
+    # We need only stmt info.
     return [str(pair.stmt) for pair in pseudo_result]
 
 
-class DeclOnlyType(unittest.TestCase):
+class CommonTestCase(unittest.TestCase):
+
+    def check(self, inp, expected):
+        self.assertEqual([
+            str(stmt) for stmt in expected], compile(inp))
+
+
+class DeclOnlyTypeTest(CommonTestCase):
 
     def test_integer(self):
-        inp = "var myVariable: Integer"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="myVariable", type_=ast.Name("Integer"),
-                expr=None), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="myVariable", type_=ast.Name("Integer"),
+            expr=None), )
+        self.check("var myVariable: Integer", expected)
 
     def test_string(self):
-        inp = "var a: String"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=ast.Name("String"),
-                expr=None), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=ast.Name("String"),
+            expr=None), )
+        self.check("var a: String", expected)
 
     def test_c_intfast8(self):
-        inp = "var a1: c#IntFast8"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a1", type_=ast.ModuleMember(
-                    name=cdefs.CMODULE_NAME,
-                    member=ast.Name("IntFast8")),
-                expr=None), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a1", type_=ast.ModuleMember(
+                name=cdefs.CMODULE_NAME,
+                member=ast.Name("IntFast8")),
+            expr=None), )
+        self.check("var a1: c#IntFast8", expected)
 
     def test_c_intfast32(self):
-        inp = "var a1: c#IntFast32"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a1", type_=ast.ModuleMember(
-                    name=cdefs.CMODULE_NAME,
-                    member=ast.Name("IntFast32")),
-                expr=None), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a1", type_=ast.ModuleMember(
+                name=cdefs.CMODULE_NAME,
+                member=ast.Name("IntFast32")),
+            expr=None), )
+        self.check("var a1: c#IntFast32", expected)
 
     def test_c_uintfast8(self):
-        inp = "var a1: c#UIntFast8"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a1", type_=ast.ModuleMember(
-                    name=cdefs.CMODULE_NAME,
-                    member=ast.Name("UIntFast8")),
-                expr=None), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a1", type_=ast.ModuleMember(
+                name=cdefs.CMODULE_NAME,
+                member=ast.Name("UIntFast8")),
+            expr=None), )
+        self.check("var a1: c#UIntFast8", expected)
 
     def test_c_uintfast32(self):
-        inp = "var a1: c#UIntFast32"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a1", type_=ast.ModuleMember(
-                    name=cdefs.CMODULE_NAME,
+        expected = (ast.Decl(
+            name="a1", type_=ast.ModuleMember(
+                name=cdefs.CMODULE_NAME,
                     member=ast.Name("UIntFast32")),
-                expr=None), )]
-        self.assertEqual(out, compile(inp))
+            expr=None), )
+        self.check("var a1: c#UIntFast32", expected)
 
 
-class DeclOnlyExpr(unittest.TestCase):
+class DeclOnlyExprTest(CommonTestCase):
 
     def test_integer(self):
-        inp = "var a = 1"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=None, expr=ast.Integer(
-                    literal="1")), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=None, expr=ast.Integer(
+                literal="1")), )
+        self.check("var a = 1", expected)
 
     def test_integer_list1(self):
-        inp = "var a = 1 + 1"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=None, expr=[
-                    "+",
-                    ast.Integer(literal="1"),
-                    ast.Integer(literal="1")
-                ]), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=None, expr=[
+                "+",
+                ast.Integer(literal="1"),
+                ast.Integer(literal="1")
+            ]), )
+        self.check("var a = 1 + 1", expected)
 
     def test_integer_list2(self):
-        inp = "var a = (2 + 9) * 1"
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=None, expr=[
-                    "*",
-                    [
-                        "+",
-                        ast.Integer(literal="2"),
-                        ast.Integer(literal="9")
-                    ],
-                    ast.Integer(literal="1")
-                ]), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=None, expr=[
+                "*",
+                [
+                    "+",
+                    ast.Integer(literal="2"),
+                    ast.Integer(literal="9")
+                ],
+                ast.Integer(literal="1")
+            ]), )
+        self.check("var a = (2 + 9) * 1", expected)
 
     def test_string(self):
-        inp = 'var a = "Hello, world!"'
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=None, expr=ast.String(
-                    literal="Hello, world!")), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=None, expr=ast.String(
+                literal="Hello, world!")), )
+        self.check('var a = "Hello, world!"', expected)
 
     def test_c_intfast8(self):
-        inp = 'var a = c#IntFast8(0)'
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=None, expr=ast.CIntFast8(
-                    literal="0")), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=None, expr=ast.CIntFast8(
+                literal="0")), )
+        self.check("var a = c#IntFast8(0)", expected)
 
     def test_c_uintfast8(self):
-        inp = 'var a = c#UIntFast8(0)'
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=None, expr=ast.CUIntFast8(
-                    literal="0")), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=None, expr=ast.CUIntFast8(
+                literal="0")), )
+        self.check("var a = c#UIntFast8(0)", expected)
 
     def test_c_intfast32(self):
-        inp = 'var a = c#IntFast32(0)'
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=None, expr=ast.CIntFast32(
-                    literal="0")), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=None, expr=ast.CIntFast32(
+                literal="0")), )
+        self.check("var a = c#IntFast32(0)", expected)
 
     def test_c_uintfast32(self):
-        inp = 'var a = c#UIntFast32(0)'
-        out = [str(stmt) for stmt in (
-            ast.Decl(
-                name="a", type_=None, expr=ast.CUIntFast32(
-                    literal="0")), )]
-        self.assertEqual(out, compile(inp))
+        expected = (ast.Decl(
+            name="a", type_=None, expr=ast.CUIntFast32(
+                literal="0")), )
+        self.check("var a = c#UIntFast32(0)", expected)
 
 
 if __name__ == "__main__":
