8069e28b5c2f586deaca28898b976952ffa69045
Pavlo Kapyshin <i@93z.org>
2017-08-09 17:56:52 +0200

    Add signatures

--- madgoshka/Adrian/CGenTests/Main0.hs
+++ madgoshka/Adrian/CGenTests/Main0.hs
@@ -5,9 +5,11 @@ import Test.HUnit
 import qualified Adrian.CGen as C
 
 
+test1 :: Test
 test1 = TestCase $ do
     assertEqual "" "int main(void) {\nreturn 0;\n}"
         (C.gens [C.main0 [C.Return (C.Val "0" C.Int)]])
 
 
+tests :: Test
 tests = TestList [test1]
