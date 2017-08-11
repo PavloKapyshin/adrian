9194c7273366f4cb6b5ca5d5a5e4ba362b2152fa
Pavlo Kapyshin <i@93z.org>
2017-08-11 20:59:54 +0200

    Improve testing helpers

--- madgoshka/CGenTestRunner.hs
+++ madgoshka/CGenTestRunner.hs
@@ -1,16 +1,8 @@
 module Main where
 
-import Test.HUnit (Counts(Counts), runTestTT)
-import System.Exit (ExitCode(ExitSuccess, ExitFailure), exitWith)
-
 import Adrian.CGenTests (tests)
+import Adrian.TestUtils (runTests)
 
 
 main :: IO ()
-main = do
-    Counts _ _ errors failures <- runTestTT tests
-    let (msg, code) = case (errors, failures) of
-         (0, 0) -> ("OK", ExitSuccess)
-         (_, _) -> ("FAILED", ExitFailure 1)
-    putStrLn msg
-    exitWith code
+main = runTests tests
