84694b3f02112e38effc4101e8f86879c19ad0ad
Elliptica <elliptica.magnificum@gmail.com>
2017-08-14 20:21:58 +0300

    Add :genc command to repl

--- margolith/test/repl.py
+++ margolith/test/repl.py
@@ -22,7 +22,6 @@ class REPL(cmd.Cmd):
 
     def do_eval(self, inp):
         try:
-            self.input_.append(inp)
             print(
                 margo.compile_repl(
                     "\n".join(self.input_), contexts=self.contexts))
@@ -31,13 +30,20 @@ class REPL(cmd.Cmd):
         except Exception as e:
             traceback.print_exc(chain=False)
 
+    def command(self, command):
+        if command == "genc":
+            self.do_eval(self.input_)
+
     def do_exit(self, arg):
         sys.exit(0)
 
     def default(self, inp):
         if inp == "EOF":
             return True
-        self.do_eval(inp)
+        if inp.startswith(":"):
+            self.command(inp.split(":")[1])
+        else:
+            self.input_.append(inp)
 
     do_quit = do_exit
     do_q = do_exit
