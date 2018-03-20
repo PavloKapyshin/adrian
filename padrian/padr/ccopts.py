3fe65e8a7af024951f9a80a813fb9e685aa0f8bf
Elliptica <elliptica.magnificum@gmail.com>
2018-03-20 15:15:21 +0200

    Make ccopts more user-friendly

--- padrian/padr/ccopts.py
+++ padrian/padr/ccopts.py
@@ -9,8 +9,12 @@ def make(cc, lib_path, out_file):
         if "source" in v:
             files.append(v["source"])
     out = ".".join([os.path.splitext(out_file.name)[0], "out"])
-    return "{cc} -o {out} {lib_paths} {files} {src_file}".format_map({
-        "cc": cc, "out": out,
-        "lib_paths": " ".join(["-I" + lib for lib in lib_path]),
-        "files": " ".join(files),
-        "src_file": out_file.name})
+    if files:
+        return "{cc} -o {out} {lib_paths} {files} {src_file}".format_map({
+            "cc": cc, "out": out,
+            "lib_paths": " ".join(["-I" + lib for lib in lib_path]),
+            "files": " ".join(files),
+            "src_file": out_file.name})
+    return "{cc} -o {out} {src_file}".format_map({
+            "cc": cc, "out": out,
+            "src_file": out_file.name})
