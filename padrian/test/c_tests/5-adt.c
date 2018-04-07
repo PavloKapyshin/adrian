9f854ad7ce3d51fe7eb8628b6a137261b4d22e01
Elliptica <elliptica.magnificum@gmail.com>
2018-04-07 13:49:52 +0300

    Update test

--- padrian/test/c_tests/5-adt.c
+++ padrian/test/c_tests/5-adt.c
@@ -1,60 +1,79 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <adrian_c.h>
-struct ud4fc83Some {
-void* ud4fc83data;
+struct u52d71aSome {
+void* u52d71adata;
 uint_fast64_t type_tag;
 };
-struct ud4fc83None {
+struct u52d71aNone {
 uint_fast64_t type_tag;
 };
-struct ud4fc83None* ud4fc83None__init__() {
-struct ud4fc83None* self = malloc(sizeof(struct ud4fc83None));
+struct u52d71aNone* u52d71aNone__init__() {
+struct u52d71aNone* self = malloc(sizeof(struct u52d71aNone));
 self->type_tag = 52;
 return self;
 }
-struct ud4fc83None* ud4fc83None__copy__(struct ud4fc83None* self) {
-struct ud4fc83None* new = malloc(sizeof(struct ud4fc83None));
+struct u52d71aNone* u52d71aNone__copy__(struct u52d71aNone* self) {
+struct u52d71aNone* new = malloc(sizeof(struct u52d71aNone));
 new->type_tag = 52;
 return new;
 }
-void ud4fc83None__deinit__(struct ud4fc83None* self) {
+void u52d71aNone__deinit__(struct u52d71aNone* self) {
 free(self);
 }
-union ud4fc83Maybe {
-struct ud4fc83Some* f0;
-struct ud4fc83None* f1;
+union u52d71aMaybe {
+struct u52d71aSome* f0;
+struct u52d71aNone* f1;
 };
 int main(void) {
-union ud4fc83Maybe* ud4fc83b = malloc(sizeof(union ud4fc83Maybe));
+union u52d71aMaybe* u52d71ab = malloc(sizeof(union u52d71aMaybe));
 struct IntFast8* t0 = IntFast8__init__(1);
-struct ud4fc83Some* i4_self = malloc(sizeof(struct ud4fc83Some));
-i4_self->ud4fc83data = IntFast8__copy__(t0);
+struct u52d71aSome* i4_self = malloc(sizeof(struct u52d71aSome));
+i4_self->u52d71adata = IntFast8__copy__(t0);
 i4_self->type_tag = 51;
-ud4fc83b->f0 = i4_self;
-if (((struct ud4fc83Some*)(ud4fc83b->f0))->type_tag == 51) {
-IntFast8__deinit__(((struct ud4fc83Some*)(ud4fc83b->f0))->ud4fc83data);
-free(ud4fc83b->f0);
+u52d71ab->f0 = i4_self;
+if (((struct u52d71aSome*)(u52d71ab->f0))->type_tag == 51) {
+IntFast8__deinit__(((struct u52d71aSome*)(u52d71ab->f0))->u52d71adata);
+free(u52d71ab->f0);
 }
-else if (((struct ud4fc83Some*)(ud4fc83b->f1))->type_tag == 52) {
-ud4fc83None__deinit__(ud4fc83b->f1);
+else if (((struct u52d71aSome*)(u52d71ab->f1))->type_tag == 52) {
+u52d71aNone__deinit__(u52d71ab->f1);
 }
-free(ud4fc83b);
-ud4fc83b = malloc(sizeof(union ud4fc83Maybe));
+free(u52d71ab);
+u52d71ab = malloc(sizeof(union u52d71aMaybe));
 struct IntFast8* t1 = IntFast8__init__(2);
-struct ud4fc83Some* i8_self = malloc(sizeof(struct ud4fc83Some));
-i8_self->ud4fc83data = IntFast8__copy__(t1);
+struct u52d71aSome* i8_self = malloc(sizeof(struct u52d71aSome));
+i8_self->u52d71adata = IntFast8__copy__(t1);
 i8_self->type_tag = 51;
-ud4fc83b->f0 = i8_self;
+u52d71ab->f0 = i8_self;
+union u52d71aMaybe* i11_new = malloc(sizeof(union u52d71aMaybe));
+if (((struct u52d71aSome*)(u52d71ab->f0))->type_tag == 51) {
+struct u52d71aSome* i11_i10_new = malloc(sizeof(struct u52d71aSome));
+i11_i10_new->u52d71adata = IntFast8__copy__(((struct u52d71aSome*)(u52d71ab->f0))->u52d71adata);
+i11_i10_new->type_tag = 51;
+i11_new->f0 = i11_i10_new;
+}
+else if (((struct u52d71aSome*)(u52d71ab->f1))->type_tag == 52) {
+i11_new->f1 = u52d71aNone__copy__(u52d71ab->f1);
+}
+union u52d71aMaybe* u52d71ac = i11_new;
 IntFast8__deinit__(t0);
 IntFast8__deinit__(t1);
-if (((struct ud4fc83Some*)(ud4fc83b->f0))->type_tag == 51) {
-IntFast8__deinit__(((struct ud4fc83Some*)(ud4fc83b->f0))->ud4fc83data);
-free(ud4fc83b->f0);
+if (((struct u52d71aSome*)(u52d71ab->f0))->type_tag == 51) {
+IntFast8__deinit__(((struct u52d71aSome*)(u52d71ab->f0))->u52d71adata);
+free(u52d71ab->f0);
 }
-else if (((struct ud4fc83Some*)(ud4fc83b->f1))->type_tag == 52) {
-ud4fc83None__deinit__(ud4fc83b->f1);
+else if (((struct u52d71aSome*)(u52d71ab->f1))->type_tag == 52) {
+u52d71aNone__deinit__(u52d71ab->f1);
 }
-free(ud4fc83b);
+free(u52d71ab);
+if (u52d71ac->f0->type_tag == 51) {
+IntFast8__deinit__(((struct u52d71aSome*)(u52d71ac->f0))->u52d71adata);
+free(u52d71ac->f0);
+}
+else if (u52d71ac->f1->type_tag == 52) {
+u52d71aNone__deinit__(u52d71ac->f1);
+}
+free(u52d71ac);
 return 0;
 }
