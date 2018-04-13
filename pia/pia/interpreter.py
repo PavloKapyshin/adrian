dc8ff8686a5f10099435214caab469a37cfa454f
Elliptica <elliptica.magnificum@gmail.com>
2018-04-13 14:52:09 +0300

    Add more tests

--- pia/pia/interpreter.py
+++ pia/pia/interpreter.py
@@ -41,8 +41,8 @@ _dict = {
 
 
 def _eval_py_method_2arg(method_name, arg1, arg2):
-    method_name = method_name[
-        (defs.MANGLING_PREFIX_LEN+len(defs.U_STRING)):]
+    # method_name = method_name[
+    #     (defs.MANGLING_PREFIX_LEN+len(defs.U_STRING)):]
     return _dict[method_name](arg1, arg2)
 
 
